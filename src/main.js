2715732a4a23818b025f83bbafd17037a1d7c56c
ParaSLee <610033941@qq.com>
2018-07-22 21:44:12 +0800

    添加element-ui

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+// 导入element-ui
+import { Tabs, TabPane } from 'element-ui';
+
 import API from '@/api/index';
 import App from './App';
 import router from './router';
 
 import components from './components';
+
+Vue.use(Tabs);
+Vue.use(TabPane);
 
 Vue.config.productionTip = false;
 
